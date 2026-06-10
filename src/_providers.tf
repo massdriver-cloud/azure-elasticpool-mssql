729668a36ba408d4a77b99864b7e9df29f6c21a7
chrisghill <chris@massdriver.cloud>
2026-06-09 18:03:51 -0600

    remove org scoping on artdefs

--- src/_providers.tf
+++ src/_providers.tf
@@ -24,7 +24,7 @@ provider "azurerm" {
   }
 
   client_id       = var.azure_authentication.client_id
-  tenant_id       = var.azure_authentication.data.tenant_id
-  client_secret   = var.azure_authentication.data.client_secret
-  subscription_id = var.azure_authentication.data.subscription_id
+  tenant_id       = var.azure_authentication.tenant_id
+  client_secret   = var.azure_authentication.client_secret
+  subscription_id = var.azure_authentication.subscription_id
 }
